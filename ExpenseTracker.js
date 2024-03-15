c92d21eef9a90c6bc11aa22581fbf031a2f4af30
ajandhyala1 <126433576+ajandhyala1@users.noreply.github.com>
2024-03-15 19:25:02 +0000

    Aditya

--- ExpenseTracker.js
+++ ExpenseTracker.js
@@ -82,3 +82,23 @@ for (const expense of expenses) {
     dateCell.textContent = expense.date;
     deleteCell.appendChild(deleteBtn);
 }
+
+
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+
+
+  function DarkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
